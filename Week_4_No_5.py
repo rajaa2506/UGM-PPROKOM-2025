b7be285498c112d3942744f8bcdd5531f350c4fb
rajaa2506 <rajawiryaoktaviando.r@mail.ugm.ac.id>
2025-09-08 15:42:34 +0700

    Add files via upload

---
+++ Week_4_No_5.py
@@ -0,0 +1,10 @@
+nilai = int(input("Masukkan nilai ujian = "))
+ms = "Siswa lulus ujian"
+tms = "Siswa tidak lulus ujian"
+
+if (nilai >= 70):
+    print()
+    print(ms)
+else:
+    print()
+    print(tms)
