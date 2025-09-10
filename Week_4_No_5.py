f0ac9957b100819452d65df0ed149aaf7ad3a3ab
rajaa2506 <rajawiryaoktaviando.r@mail.ugm.ac.id>
2025-09-10 10:41:41 +0700

    Add files via upload

---
+++ Week_4_No_5.py
@@ -0,0 +1,42 @@
+ms = "Siswa lulus ujian"
+tms = "Siswa tidak lulus ujian"
+
+nilai1 = int(input("Masukkan nilai ujian siswa 1 = "))
+if (nilai1 >= 70):
+    print(ms)
+    print()
+else:
+    print(tms)
+    print()
+    
+nilai2 = int(input("Masukkan nilai ujian siswa 2 = "))
+if (nilai2 >= 70):
+    print(ms)
+    print()
+else:
+    print(tms)
+    print()
+    
+nilai3 = int(input("Masukkan nilai ujian siswa 3 = "))
+if (nilai3 >= 70):
+    print(ms)
+    print()
+else:
+    print(tms)
+    print()
+
+nilai4 = int(input("Masukkan nilai ujian siswa 4 = "))
+if (nilai4 >= 70):
+    print(ms)
+    print()
+else:
+    print(tms)
+    print()
+    
+nilai5 = int(input("Masukkan nilai ujian siswa 5 = "))
+if (nilai5 >= 70):
+    print(ms)
+    print()
+else:
+    print(tms)
+    print()
